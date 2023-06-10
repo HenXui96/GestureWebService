40f8945afd2cc63fb00c3508a2dd89f58487b7a0
nhidm <nhidm@vng.com.vn>
2023-06-10 19:26:45 +0700

    .

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,1 @@
-from rest_framework import serializers
 from .models import ImageModel
-
-class ImageModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ImageModel
-        fields = ['image']
--- api/views.py
+++ api/views.py
@@ -1,31 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import ImageModel
 from .serializers import ImageModelSerializer
 
 # Create your views here.
 from GestureLearner import *
-
-
-class ImageModelAPIView(APIView):
-    def get(self, request):
-        mymodels = ImageModel.objects.all()
-        serializer = ImageModelSerializer(mymodels, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ImageModelSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            result = self.make_predict(serializer.data.image)
-            return Response(result, status=201)
-        return Response(serializer.errors, status=400)
-    
-    def make_predict(self, data):
-        return predict(data)
     
 
 def make_predict(request):
