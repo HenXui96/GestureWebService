426d79082ad78029f01a7b58cfc750eaab221533
nhidm <nhidm@vng.com.vn>
2023-06-10 19:36:29 +0700

    .

--- api/views.py
+++ api/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import ImageModel
-from .serializers import ImageModelSerializer
 
 # Create your views here.
 from GestureLearner import *
